9ad8b7553fb67c031daa6160fcb8b408a8749940
Pham-Gia-Bao1 <134178887+Pham-Gia-Bao1@users.noreply.github.com>
2023-12-29 14:49:26 +0700

    Thu/contact us view (#14)
    * fix homepage
    
    * contact us
    
    * remain name contactUs
    
    * contactUs
    
    * finish contactUs
    
    * contact us
    
    ---------
    
    Co-authored-by: thu <thu.vo25@student.passerellesnumeriques.org>

--- root/CSS/About.css.php
+++ root/CSS/About.css.php
@@ -1,24 +1,36 @@
 <style>
-    
-
 * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
 }
+.content h5{
+    color: #267BFC;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+    text-align: center;
+}
+.content h3{
+    color:#1F2B6C;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-weight: bold;
+    text-align: center;
+}
+
 .heading {
     padding:20px;
     width:100%;
     height:500px;
     background-position: center center;
     background-size:cover;
-    background-image: url(https://img.freepik.com/free-photo/young-pretty-stylish-smiling-woman-having-fun-city-park-positive-emotional-wearing-yellow-top-striped-mini-skirt-pink-sunglasses-summer-style-fashion-trend-positive-emotion_285396-5310.jpg?w=996&t=st=1700065396~exp=1700065996~hmac=15e899b8e10af7200f401bb452578208a17ed4cd2f5fc525d7ec94d8f61465ed)
+    background-image: url(https://img.freepik.com/free-photo/medical-staff-doctors-concept-young-smiling-female-doctor-healthcare-worker-white-coat-st_1258-121749.jpg?w=1380&t=st=1703518963~exp=1703519563~hmac=085285af120488119787f579e01eb78eaaad2ded52d4ad25b90a734f3b22f3b2)
 }
 .text{
     padding-top: 20px;
     margin-top:10%;
 }
 .text hr {
+    color: #fff;
     margin-top:10px;
 }
 .animate-charcter {
@@ -38,20 +50,25 @@
     font-size: 60px;
 }
 .contact h5 {
-    color: #002661;
+    color:#267BFC;
     padding-bottom: 10px;
+    font-size: 20px;
+    font-weight: bold;
 }
 .contact h6 {
     color: #002661;
     padding-bottom: 10px;
+    font-size: 15px;
+    font-weight: bold;
 }
 .contact {
     padding:10px;
     margin:10px;
+    /* font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; */
 }
-form {
+/* form {
     margin: 100px;
-}
+} */
 
 .input-field {
     position: relative;
@@ -99,7 +116,11 @@ input {
     padding: 5%;
     box-shadow: 7px 4px 7px 0px #888888;
 }
-
+.sendMail h6 {
+    color:#267BFC;
+    font-size: 18px;
+    font-weight: bold;
+}
 
 button {
     display: block;
@@ -146,4 +167,86 @@ button {
     .contactExpert h5 {
         color: #1F2B6C;
     }
+
+/*----  Main Style  ----*/
+#cards_landscape_wrap-2{
+  text-align: center;
+  /* background: #F7F7F7; */
+}
+#cards_landscape_wrap-2 a{
+  text-decoration: none;
+  outline: none;
+}
+#cards_landscape_wrap-2 .card-flyer {
+  border-radius: 5px;
+}
+#cards_landscape_wrap-2 .card-flyer .image-box{
+  background: #ffffff;
+  overflow: hidden;
+  box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.50);
+  border-radius: 5px;
+}
+#cards_landscape_wrap-2 .card-flyer .image-box img{
+  -webkit-transition:all .9s ease; 
+  -moz-transition:all .9s ease; 
+  -o-transition:all .9s ease;
+  -ms-transition:all .9s ease; 
+  width: 100%;
+  height: 200px;
+}
+#cards_landscape_wrap-2 .card-flyer:hover .image-box img{
+  opacity: 0.7;
+  -webkit-transform:scale(1.15);
+  -moz-transform:scale(1.15);
+  -ms-transform:scale(1.15);
+  -o-transform:scale(1.15);
+  transform:scale(1.15);
+}
+#cards_landscape_wrap-2 .card-flyer .text-box{
+  text-align: center;
+}
+#cards_landscape_wrap-2 .card-flyer .text-box .text-container{
+  padding: 30px 18px;
+}
+#cards_landscape_wrap-2 .card-flyer{
+  background: #FFFFFF;
+  margin-top: 50px;
+  -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  -ms-transition: all 0.2s ease-in;
+  -o-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.40);
+}
+#cards_landscape_wrap-2 .card-flyer:hover{
+  background: #fff;
+  box-shadow: 0px 15px 26px rgba(0, 0, 0, 0.50);
+  -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  -ms-transition: all 0.2s ease-in;
+  -o-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+  margin-top: 50px;
+}
+#cards_landscape_wrap-2 .card-flyer .text-box p{
+  margin-top: 10px;
+  margin-bottom: 0px;
+  padding-bottom: 0px; 
+  font-size: 14px;
+  letter-spacing: 1px;
+  color: #000000;
+}
+#cards_landscape_wrap-2 .card-flyer .text-box h6{
+  margin-top: 0px;
+  margin-bottom: 4px; 
+  font-size: 15px;
+  font-weight: bold;
+  /* text-transform: uppercase; */
+  font-family: 'Roboto Black', sans-serif;
+  letter-spacing: 1px;
+  color: #00acc1;
+}
+
+
+
 </style>
