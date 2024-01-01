ef02bcac19634bc9734c85de733468f3e945bf32
Pham-Gia-Bao1 <134178887+Pham-Gia-Bao1@users.noreply.github.com>
2024-01-01 12:12:01 +0700

    Thu/news view (#17)
    * news
    
    * news
    
    * news
    
    ---------
    
    Co-authored-by: thu <thu.vo25@student.passerellesnumeriques.org>

---
+++ Controller/NewsController.php
@@ -0,0 +1,13 @@
+<?php
+require "../Database/database.php";
+include("../Model/NewsModel.php");
+include_once("../Model/AccountModel.php");
+
+// $news = new News();
+// $news->get_all_news();
+// print_r(($news));
+
+
+
+require "../View/News/NewsView.php";
+?>
