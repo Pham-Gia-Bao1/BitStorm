f156a1fd1e9093dd6dd021eaabe3e2290d361a34
Viet My <Ziettmy@gmail.com>
2024-01-03 00:16:09 +0700

    update PostController

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -1,5 +1,41 @@
 <?php
-include("../Model/PostModel.php");
-include_once("../Model/AccountModel.php");
-include("../View/Post/PostView.php");
+$cookie_name = "User";
+if (isset($_COOKIE[$cookie_name])) {
+
+    include("../Model/PostModel.php");
+    include_once("../Model/AccountModel.php");
+    
+    $Post= new Post();
+    $posts=$Post->GetAllPostsAndUserName_Img();
+
+    
+    
+    // var_dump($posts);
+    $account = new Account();
+    $nameAndImg=$account->get_name_and_img_user();
+
+    // xử lí đăng bài
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (!empty($_POST['content'])){
+        $isAnonymous= isset($_POST['isAnonymous'])?($_POST['isAnonymous']):0;
+        $id=$Post->get_id($nameAndImg[0]);
+        $likeCount=0;
+        $content =htmlspecialchars($_POST['content']); 
+        $post=[
+            "userid" => $id,
+            "isAnonymous"=>$isAnonymous,
+            "likeCount"=> $likeCount,
+            "content" => $content,
+        ];
+        
+        $Post->CreatePost($post);
+        // echo "đã creatPost";
+        }
+
+    }
+    include("../View/Post/PostView.php");
+}else{
+    echo "<script> alert ('Vui lòng đăng nhập') </script>";
+}
+
 ?>
