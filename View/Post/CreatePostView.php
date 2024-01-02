6499ea0801f4e0e2b89b8b7fe387064a8ec35ed3
Viet My <Ziettmy@gmail.com>
2024-01-03 00:45:59 +0700

    update ViewPost l2

--- View/Post/CreatePostView.php
+++ View/Post/CreatePostView.php
@@ -1,14 +1,14 @@
 <div id="modalPost" class="modal">
+<form action="" method="post">
+    <div id="modalPost" class="modal">
     <div class="modal-content" id="model_post">
         <div class="modal-header">
             <h2 class="title">Tạo bài viết</h2>
-            <span class="close" onclick="closeModal()">&times;</span>
-        </div>
-        <div class="modal-body">
             <div class=" status-post-user">
                 <div><p style="margin-bottom: 0;">Đăng ẩn danh</p></div>
                 <label class="switch">
                     <input type="checkbox">
+                    <input type="checkbox" name="isAnonymous" value="1">
                     <span class="anonymous-slider"></span>
                 </label>
             </div>
@@ -23,15 +23,22 @@
                     echo $name;
                 }
                 ?></p>
+
+                <img src="<?php echo $nameAndImg[1]?>" alt="anh dai dien"   style="border-radius: 50%; width: 40px; height: 40px;" />
+                <p class="name-user mb-0 mx-2" id=""><?php echo $nameAndImg[0]?></p>
             </div>  
 
 
             <textarea class="content-modal mt-3" placeholder="Bạn viết gì đi..." oninput="autoResize(this)"></textarea>
+            <textarea class="content-modal mt-3" name="content" placeholder="Bạn viết gì đi..." oninput="autoResize(this)"></textarea>
         </div>
         <div>
             <hr>
             <button type="button" onclick="CreatePost(event)">Đăng bài viết</button>
+            <button type="submit">Đăng bài viết</button>
         </div>
 
     </div>
 </div>
+    </div>
+</form>
--- View/Post/PostView.php
+++ View/Post/PostView.php
@@ -11,80 +11,89 @@ include("../root/CSS/Post.css.php");
     <div class="row card-post">
         <div class="row post-bar box_search">
             <div class="col-1">
-                <img src="https://didongviet.vn/dchannel/wp-content/uploads/2023/08/che-do-an-danh-la-gi-1-didongviet.jpg" alt="anh dai dien" id="img" class="w-100" style="border-radius: 50%" />
+                <!--  -->
+                <img src="<?php echo $nameAndImg[1]?>" alt="anh dai dien" id="img" class="w-100" style="border-radius: 50%; height:50px" />
             </div>
             <div class="col search">
                 <input type="text" class="search__input" placeholder="Hôm nay, cậu ổn không?" onclick="openModal()">
             </div>
         </div>
-      
-
-            <div class="post">
-               
-                    <div class="post-row1">
-                        <div class="post-avt me-3">
-                            <img src="https://demoda.vn/wp-content/uploads/2022/03/anh-cute-meo-le-luoi.jpg" alt="anh dai dien" id="img" style="border-radius: 50%; width: 40px; height: 40px;" />
-                        </div>
-                        <div class="post-name">
-                            <p class="name-user">Người dùng ẩn danh</p>
-                            <p class="hours-posted">@Math.Round(@gap_hour) h trước</p>
-                        </div>
-                    </div>
-               
-                    <div class="post-row1">
-                        <div class="post-avt me-3">
-                            <img src="@post.User.Url" alt="anh dai dien" id="img" style="border-radius: 50%; width: 40px; height: 40px;" />
-                        </div>
-                        <div class="post-name">
-                            <p class="name-user">@post.User.Name</p>
-                            <p class="hours-posted">@Math.Round(@gap_hour) h trước</p>
-                        </div>
-                    </div>
-                
-                <div class="content-post">
-                    <p>
-                        @post.Content
-                    </p>
-                </div>
-                <div class="emotion">
-                    <div class="con-like">
-                        <input class="like" type="checkbox" title="like">
-                        <div class="checkmark">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="outline" viewBox="0 0 24 24">
-                                <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"></path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="filled" viewBox="0 0 24 24">
-                                <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z"></path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="100" width="100" class="celebrate">
-                                <polygon class="poly" points="10,10 20,20"></polygon>
-                                <polygon class="poly" points="10,50 20,50"></polygon>
-                                <polygon class="poly" points="20,80 30,70"></polygon>
-                                <polygon class="poly" points="90,10 80,20"></polygon>
-                                <polygon class="poly" points="90,50 80,50"></polygon>
-                                <polygon class="poly" points="80,80 70,70"></polygon>
-                            </svg>
-                        </div>
-                    </div>
-                    <i class="far fa-comment icon_comment"></i>
-                </div>
-                <div>
-                    <p style="margin-bottom: 0;"><span style="font-weight: 500; margin-bottom: 0;">@post.LikeCount</span> lượt thích</p>
-                    <p>...</p>
-                </div>
-
-                <div class="container-comment">
-                </div>
-                <div class="input-add-comment">
-
-                    <input placeholder="Thêm bình luận" class="input-field input_comment" type="text">
-                    <label for="input-field" class="input-label" >Bình luận</label>
-                    <button >thêm bình luận</button>
-                    <span class="input-highlight"></span>
-                </div>
-
-            </div>
         
+        <?php 
+
+        foreach($posts as $post):?>
+        <div class="post">
+            
+            <div class="post-row1">
+                    <div class="post-avt me-3">
+                        <img src="https://demoda.vn/wp-content/uploads/2022/03/anh-cute-meo-le-luoi.jpg" alt="anh dai dien" id="img" style="border-radius: 50%; width: 40px; height: 40px;" />
+                    </div>
+                    <div class="post-name">
+                        <p class="name-user"><?php echo $post['name']?></p>
+                        <p class="hours-posted"><?php echo $post['timeElapsed']?></p>
+                    </div>
+            </div>
+            <div class="content-post">
+                <p>
+                <?php echo $post['content']?>
+                </p>
+            </div>
+            <div class="emotion">
+                <div class="con-like">
+                    <input class="like" type="checkbox" title="like" name="">
+                    <div class="checkmark">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="outline" viewBox="0 0 24 24">
+                            <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"></path>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="filled" viewBox="0 0 24 24">
+                            <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z"></path>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="100" width="100" class="celebrate">
+                            <polygon class="poly" points="10,10 20,20"></polygon>
+                            <polygon class="poly" points="10,50 20,50"></polygon>
+                            <polygon class="poly" points="20,80 30,70"></polygon>
+                            <polygon class="poly" points="90,10 80,20"></polygon>
+                            <polygon class="poly" points="90,50 80,50"></polygon>
+                            <polygon class="poly" points="80,80 70,70"></polygon>
+                        </svg>
+                    </div>
+                </div>
+                <i class="far fa-comment icon_comment"></i>
+            </div>
+            <!-- lượt tym -->
+            <div>
+                <p style="margin-bottom: 0;"><span style="font-weight: 500; margin-bottom: 0;"><?php echo $post['like_count']?></span> lượt thích</p>
+                <p>...</p>
+            </div>
+            <!--add cmt  -->
+            <?php 
+            $comments=$post['comment'];
+            if (!empty($comments)):
+            ?>
+                <?php foreach ($comments as $comment):?>
+                <div class="post-comment m-2">
+                    <div class="post-comment-avt me-3">
+                        <img src="meow.jpg" alt="" class="avt-post">
+                    </div>
+                    <div class="post-content-comment">
+                        <p class="name-post"><?php echo $comment['author']?></p>
+                        <p class="content"><?php echo $comment['content']?></p>
+                    
+                    </div>
+                </div>
+                <?php endforeach;?>
+            <?php 
+            endif;?>
+            <!-- end -->
+            <div class="input-add-comment">
+              
+              <input placeholder="" class="input-field input_comment" type="text">
+              <label for="input-field" class="input-label">Bình luận</label>
+              <span class="input-highlight"></span>
+            </div>
+
+        </div>
+        <?php endforeach;?>
     </div>
 </div>
 
