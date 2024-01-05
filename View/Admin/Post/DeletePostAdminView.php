2c2226f077252a9c4533618f23b613d4a304b029
Viet My <Ziettmy@gmail.com>
2024-01-06 02:40:58 +0700

    Add view Post Admin

---
+++ View/Admin/Post/DeletePostAdminView.php
@@ -0,0 +1,22 @@
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deleteAdminModal">
+  Xóa
+</button>
+<!-- button xóa -->
+<!-- Modal -->
+<div class="modal fade" id="deletePostAdminModal" tabindex="-1" aria-labelledby="deleteAdminModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="deleteAdminModalLabel">Delete</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h1>Delete</h1>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ View/Admin/Post/PostAdminView.php
@@ -0,0 +1,61 @@
+<?php
+include("../View/Admin/Layout/SideBar.view.php");
+include("../root/CSS/Admin/Post.css.php");
+include("../View/Admin/Post/UpdatePostAdminView.php");
+include("../View/Admin/Post/DeletePostAdminView.php");
+?>
+?>
+<div class="main">
+    <div class="create">  
+        <button type="button" class="btn btn-primary create-post_admin" data-bs-toggle="modal" data-bs-target="#createAdminModal">
+            Tạo mới
+        </button>
+
+        <?php 
+        include("../View/Admin/Post/CreatePostAdminView.php");?>
+        <div class="show-table">
+            <table class="table table-hover table-striped ">
+                <thead class="">
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">user_id</th>
+                        <th scope="col">Lượt tym</th>
+                        <th scope="col">Ẩn danh</th>
+                        <th scope="col">Nội dung</th>
+                        
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($posts as $post):?>
+                    <tr>
+                        <td><?php echo $post['id']?></td>
+                        <td><?php echo $post['user_id']?></td>
+                        <td><?php echo $post['like_count']?></td>
+                        <td><?php echo $post['isAnonymous']?></td>
+                        <td style="max-width:600px"><?php echo $post['content']?></td>
+                        
+                        <td>
+                            <button class="btn btn-info ViewPostAdmin" data-bs-toggle="modal" data-bs-target="#viewDetailPostModal">
+
+                                <i class="fa-solid fa-eye"></i>
+                            </button>
+                            <button class="btn btn-info updatePostAdminBtn">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </button>
+                            
+                            <button class="btn btn-danger deletePostAdminBtn"<?php 
+                            // hướng dẫn cách lấy id post khi click on button
+                            ?> data-bs-toggle="modal" data-bs-target="#deletePostAdminModal">
+                                <i class="fa-solid fa-lock"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <?php endforeach;?>
+                </tbody>
+            </table>
+        </div>
+    </div>   
+
+</div>
+
